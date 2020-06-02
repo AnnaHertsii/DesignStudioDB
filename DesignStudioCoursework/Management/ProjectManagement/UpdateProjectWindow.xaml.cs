a1e44636c50d19c397df55cb1df787e836dad1d9
Anna <annahertsiy@gmail.com>
2020-06-02 13:51:25 +0300

    Verification for updating project

--- DesignStudioCoursework/Management/ProjectManagement/UpdateProjectWindow.xaml.cs
+++ DesignStudioCoursework/Management/ProjectManagement/UpdateProjectWindow.xaml.cs
@@ -75,8 +75,59 @@ namespace DesignStudioCoursework.Management.ProjectManagement
         }
 
         private void UpdateProjectButton_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateProject();
+        {           
+            if (order.Text == "")
+                order_error.Visibility = Visibility.Visible;
+            else
+            {
+                order_error.Visibility = Visibility.Hidden;
+            }
+
+            if (employee.Text == "")
+                employee_error.Visibility = Visibility.Visible;
+            else
+            {
+                employee_error.Visibility = Visibility.Hidden;
+            }
+
+            if (name.Text.Length > 50)
+                name_error.Visibility = Visibility.Visible;
+            else
+            {
+                name_error.Visibility = Visibility.Hidden;
+            }
+
+            bool isDigit = true;
+            foreach (char c in price.Text)
+            {
+                if (c < '0' || c > '9')
+                    isDigit = false;
+            }
+            if (isDigit == false)
+            {
+                price_error.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                price_error.Visibility = Visibility.Hidden;
+            }
+
+            if (style.SelectedIndex == -1)
+                style_error.Visibility = Visibility.Visible;
+            else
+            {
+                style_error.Visibility = Visibility.Hidden;
+            }
+
+            if ((interior.Text != "") && (order.Text != "") && (employee.Text != "") && (name.Text.Length < 50) && (isDigit != false) && (style.SelectedIndex != -1))
+            {
+                order_error.Visibility = Visibility.Hidden;
+                employee_error.Visibility = Visibility.Hidden;
+                name_error.Visibility = Visibility.Hidden;
+                price_error.Visibility = Visibility.Hidden;
+                style_error.Visibility = Visibility.Hidden;
+                UpdateProject();
+            }
             display.ShowProjects(datagrid);
         }
 
