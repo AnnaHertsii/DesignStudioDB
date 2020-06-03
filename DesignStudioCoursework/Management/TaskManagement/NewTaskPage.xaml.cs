a98c8e12356b4cd83ca6d7171e24308da8897b64
Anna <annahertsiy@gmail.com>
2020-06-03 16:24:42 +0300

    Finished new task

--- DesignStudioCoursework/Management/TaskManagement/NewTaskPage.xaml.cs
+++ DesignStudioCoursework/Management/TaskManagement/NewTaskPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,77 @@ namespace DesignStudioCoursework.Management.TaskManagement
         {
             ProjectsWindow projects = new ProjectsWindow(project);
             projects.Show();
+        }
+
+        private void AddTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddTask();
+        }
+
+        public void AddTask()
+        {
+            try
+            {
+                int task_id = MaxID() + 1;
+                string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                int Status_id = statuscombo.SelectedIndex + 1;
+                int Employee_id = employeecombo.SelectedIndex + 1;
+
+                string formattedstart = null;
+                string formattedend = null;
+                DateTime? startDate = start_date.SelectedDate;
+                DateTime? endDate = end_date.SelectedDate;
+                if (startDate.HasValue && endDate.HasValue)
+                {
+                    formattedstart = startDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                    formattedend = endDate.Value.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+                }
+
+                string strSQL = string.Format("INSERT INTO [Task](Task_ID, Task_name, Description, Start_date, End_date, Employee_Ref, Project_Ref, Task_status_Ref) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')",
+                    task_id, name.Text, description.Text, formattedstart, formattedend, Employee_id, getProjectID(), Status_id);
+
+                SqlCommand myCommand = new SqlCommand(strSQL, connection);
+                myCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Нову задачу створено!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public int getProjectID()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            string strSQL = string.Format("SELECT TOP 1 Project_ID FROM [Design Project] WHERE Project_name = '{0}'", project.Text);
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            SqlDataReader reader = myCommand.ExecuteReader();
+            string st = null;
+            if (reader.Read())
+                st = reader[0].ToString();
+            return Int32.Parse(st);
+        }
+
+        public int MaxID()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            string strSQL = "SELECT MAX(Task_ID) FROM [Task]";
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            SqlDataReader reader = myCommand.ExecuteReader();
+            string st = null;
+            if (reader.Read())
+                st = reader[0].ToString();
+            return Int32.Parse(st);
         }
     }
 }
