412a13594c764c5a293cf1595c6b3e9a030d75f6
Anna <annahertsiy@gmail.com>
2020-06-04 01:07:36 +0300

    Deleted items (but without FK!)

--- DesignStudioCoursework/Management/TaskManagement/ItemsPage.xaml.cs
+++ DesignStudioCoursework/Management/TaskManagement/ItemsPage.xaml.cs
@@ -51,5 +51,87 @@ namespace DesignStudioCoursework.Management.TaskManagement
             if (task_id.Text != "") task_ref = Int32.Parse(task_id.Text);
             display.ShowItemsForTask(DataGridItem, task_ref);
         }
+
+        private void DeleteItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteItem();
+            int task_ref = 0;
+            if (task_id.Text != "") task_ref = Int32.Parse(task_id.Text);
+            display.ShowItemsForTask(DataGridItem, task_ref);
+        }
+    
+        public void DeleteItem()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            int SelectedId = CurrentID();
+            string strSQL = string.Format("DELETE [Item] WHERE Item_ID = '{0}'", SelectedId);
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            myCommand.ExecuteNonQuery();
+
+            MessageBox.Show("Предмет видалено!");
+        }
+
+        public string GetSelectedCellValue(int index)
+        {
+            DataGridCellInfo cellInfo = DataGridItem.SelectedCells[index];
+            if (cellInfo == null) return null;
+
+            DataGridBoundColumn column = cellInfo.Column as DataGridBoundColumn;
+            if (column == null) return null;
+
+            FrameworkElement element = new FrameworkElement() { DataContext = cellInfo.Item };
+            BindingOperations.SetBinding(element, TagProperty, column.Binding);
+
+            return element.Tag.ToString();
+        }
+
+        public int getEmployeeID()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            string strSQL = string.Format("SELECT TOP 1 Employee_ID FROM [Employee] WHERE Name = '{0}'", GetSelectedCellValue(4));
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            SqlDataReader reader = myCommand.ExecuteReader();
+            string st = null;
+            if (reader.Read())
+                st = reader[0].ToString();
+            return Int32.Parse(st);
+        }
+
+        public int getProjectID()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            string strSQL = string.Format("SELECT TOP 1 Project_ID FROM [Design Project] WHERE Project_name = '{0}'", GetSelectedCellValue(5));
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            SqlDataReader reader = myCommand.ExecuteReader();
+            string st = null;
+            if (reader.Read())
+                st = reader[0].ToString();
+            return Int32.Parse(st);
+        }
+
+        public int CurrentID()
+        {
+            string connectionString = @"Data Source=DESKTOP-O22ROGE;Initial Catalog=DesignStudio;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string Name = GetSelectedCellValue(0);
+            string Amount = GetSelectedCellValue(2);
+            SqlCommand command = new SqlCommand();
+            string strSQL = string.Format("SELECT Item_ID FROM [Item] WHERE Item_name = '{0}' AND Item_amount = '{1}'", Name, Amount);
+            SqlCommand myCommand = new SqlCommand(strSQL, connection);
+            SqlDataReader reader = myCommand.ExecuteReader();
+            string st = null;
+            if (reader.Read())
+                st = reader[0].ToString();
+            return Int32.Parse(st);
+        }
     }
 }
